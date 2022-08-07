1e8f511580baba18725f6236e86c850e04265584
Tyler Beck <tbeck202@gmail.com>
2022-08-07 10:09:50 -0700

    Add project files.

---
+++ Controllers/PagesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCpractice2.Models;
+
+
+namespace MVCpractice2.Controllers
+{
+    public class PagesController : Controller
+    {
+        private static List<VisitorViewModel> visitors = new List<VisitorViewModel>();
+        public IActionResult PageOne()
+        {
+            return View(visitors);
+        }
+        public IActionResult PageTwo()
+        {
+            return View();
+        }
+
+        public IActionResult PageThree()
+        {
+            VisitorViewModel visitorVm = new VisitorViewModel();
+
+            return View(visitorVm);
+        }
+        public IActionResult CreateVisitor(VisitorViewModel visitor)
+        {
+            visitors.Add(visitor);
+            return RedirectToAction("PageOne");
+        }
+    }
+}
---
+++ Models/VisitorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCpractice2.Models
+{
+    public class VisitorViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Comment { get; set; }
+    }
+}
